9f162b02c81f323bd012ca1877cbb54726d48076
George-byt <jmoralesvalencia750@gmail.com>
2021-02-03 17:50:11 +0000

    C language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+* main - Program that prints a text.
+*
+* Return: Alwayes 0 (Success)
+*
+*/
+int main(void)
+{
+
+puts("Programming is like building a multilingual puzzle");
+return (0);
+
+}
