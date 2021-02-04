c26e9a04e7b821fcc129182522db8fe3a802a0ac
George-byt <jmoralesvalencia750@gmail.com>
2021-02-04 20:20:58 +0000

    Variables, if, else, while.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int main(void)
+
+{
+
+int number;
+number = 48;
+
+while (number <= 57)
+
+{
+putchar(number);
+number++;
+}
+
+putchar('\n');
+
+return (0);
+}
