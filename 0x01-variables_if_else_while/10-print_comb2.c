696394267776c350deb24e7fb0361cc4a72af73b
George-byt <jmoralesvalencia750@gmail.com>
2021-02-04 23:08:59 +0000

    Variables, if, else, while.

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 01 to 99.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+
+int number_a, number_b;
+
+for (number_a = 48; number_a <= 57; number_a++)
+for (number_b = 48; number_b <= 57; number_b++)
+{
+
+putchar(number_a);
+putchar(number_b);
+
+if (number_a == 57 && number_b == 57)
+
+{
+putchar('\n');
+break;
+}
+
+putchar(',');
+putchar(' ');
+
+}
+
+return (0);
+
+}
