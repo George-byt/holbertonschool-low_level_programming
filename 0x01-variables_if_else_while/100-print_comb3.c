5efd80968f38d8bffbd9ed1e085be0f2b55f6745
George-byt <jmoralesvalencia750@gmail.com>
2021-02-05 15:43:59 +0000

    C Language

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 {
 int number_a, number_b;
 for (number_a = 48; number_a <= 56; number_a++)
-for (number_b = 49; number_b <= 57; number_b++)
+for (number_b = 57; number_b >= 49; number_b++)
 {
 putchar(number_a);
 putchar(number_b);
