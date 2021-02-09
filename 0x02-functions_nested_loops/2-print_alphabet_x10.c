cdcf6d707bc314ab3f0dcedaae346088f263225a
George-byt <jmoralesvalencia750@gmail.com>
2021-02-09 19:58:08 +0000

    Nested loops.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,32 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - Function that prints the alphabet.
- * Return: Always 0.
+ * print_alphabet_x10 - prints the alphabet 10 times.
+ * Description: uses 2 for loops to pring a to z
+ * 10 times.
+ * Return: void.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 
 {
+
 char lowercase;
 int count;
-for (count = 1; count <= 10; count++)
+
+for (count = 0; count <= 10; count++)
+
 {
+
 lowercase = 'a';
 while (lowercase <= 'z')
 {
 _putchar(lowercase);
 lowercase++;
 }
-_putchar('\n');
+
 }
+
 _putchar('\n');
 
 }
