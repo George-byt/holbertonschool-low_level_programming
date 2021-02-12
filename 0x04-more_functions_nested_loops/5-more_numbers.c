d74a80c2a8b2376fa97eaba30a9414aa80298d96
George-byt <jmoralesvalencia750@gmail.com>
2021-02-12 01:36:00 +0000

    More Nested Loops.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,21 @@ void more_numbers(void)
 
 {
 
+int a;
+int b;
+int c;
 
-
-
-
-
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '1'; b++)
+for (c = '0'; c <= '9'; c++)
+{
+if (!(b == '0'))
+if (!(b == '1' && c > '4'))
+_putchar(b);
+if (!(b == '1' && c > '4'))
+_putchar(c);
+}
+_putchar('\n');
+}
 }
