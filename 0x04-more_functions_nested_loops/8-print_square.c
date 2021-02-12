6f5ab65f675f498e7603937437a65799bbb07b86
George-byt <jmoralesvalencia750@gmail.com>
2021-02-12 19:46:03 +0000

    More Nested Loops.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+int largo;
+int ancho;
+if (size > 0)
+{
+for (ancho = 0; ancho < size; ancho++)
+{
+for (largo = 0;  largo < size; largo++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
