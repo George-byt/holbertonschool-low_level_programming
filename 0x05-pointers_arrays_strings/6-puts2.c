6b2c20dc70619582014bb601e8265a551a46c5f4
George-byt <jmoralesvalencia750@gmail.com>
2021-02-16 21:31:46 +0000

    Pointers, strings and arrays.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * puts2 - Function that prints numbers.
+ * @str: String pointer.
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+for (; *str != '\0'; str++)
+if (*str % 2 == 0)
+{
+_putchar(*str);
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void puts2(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,4 +9,8 @@ void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
 #endif
