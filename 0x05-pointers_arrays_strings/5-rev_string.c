0308c934f7726585eef890e6fdd11f94a4a9d3ec
George-byt <jmoralesvalencia750@gmail.com>
2021-02-16 17:26:22 +0000

    Pointers, strings and arrays.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,19 @@
  */
 void rev_string(char *s)
 {
+int len;
+len = 0;
 for (; *s != '\0'; s++)
 {
 _putchar(*s);
 }
-
-
-
-
-
+for (; *s != '\0'; s++)
+len++;
+s--;
+for (; len > 0; s--)
+{
+_putchar(*s);
+len--;
+}
 _putchar('\n');
 }
