c4695998870b261f20ca7bf3d18ac1f2e34f3ff6
George-byt <jmoralesvalencia750@gmail.com>
2021-02-23 16:53:18 +0000

    Even More Pointers, Arrays and Strings.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strstr - Function that locates a substring.
+ * @haystack:
+ * @needle:
+ *
+ *
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+
+
+
+
+
+
+
+}
