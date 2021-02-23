a668566c9d6b7181be5791c795c868e3141581ed
George-byt <jmoralesvalencia750@gmail.com>
2021-02-23 17:45:08 +0000

    Even More Pointer, Arrays and Strings.

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_string - Functions that sets the value of a pointer to a char.
+ * @s: Pointer.
+ * @to: Char.
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
