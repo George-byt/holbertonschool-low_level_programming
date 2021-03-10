049c957192c9fa9f3ddd80f478f60b572543cb20
George-byt <jmoralesvalencia750@gmail.com>
2021-03-10 20:32:15 +0000

    Function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * cap_string - function that capitalizes all words of a string.
+ * @s: array.
+ * Return: The array changed.
+ */
+char *cap_string(char *s)
+{
+int a;
+int c;
+a = 1;
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] >= 'A' && s[c] <= 'Z')
+a = 0;
+else if (s[c] >= '0' && s[c] <= '9')
+a = 0;
+else if ((s[c] >= 'a' && s[c] <= 'z') && a == 1)
+{
+s[c] -= 32;
+a = 0;
+}
+if (s[c] == 9 || s[c] == 10 || s[c] == 32 || s[c] == 33
+|| s[c] == 34 || s[c] == 40 || s[c] == 41
+|| s[c] == 44 || s[c] == 46 || s[c] == 59
+|| s[c] == 63 || s[c] == 123 || s[c] == 125)
+a = 1;
+}
+return (s);
+}
