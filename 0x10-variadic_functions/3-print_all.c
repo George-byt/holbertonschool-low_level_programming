2d546c2f4c55364750401009fbd56009c996a67b
George-byt <jmoralesvalencia750@gmail.com>
2021-03-12 02:57:03 +0000

    Function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,44 +1,44 @@
 #include "variadic_functions.h"
+
 /**
- * print_all - Function that prints anythig.
- * @format: The list of types of arguments.
- * Return: Nothing.
+ * print_all - prints all kinds of types
+ * @format: the list of types of arguments
+ * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
+  va_list ar;
+  int i = 0;
+  char form;
+  char *str;
 
-va_list arg;
-int i = 0;
-char form;
-char *str;
-
-va_start(arg, format);
-while (format != NULL && format[i])
-{
-form = format[i];
-switch (form)
-{
-case 'c':
-printf("%c", va_arg(arg, int));
-break;
-case 'i':
-printf("%d", va_arg(arg, int));
-break;
-case 'f':
-printf("%f", va_arg(arg, double));
-break;
-case 's':
-str = va_arg(arg, char *);
-if (str == NULL)
-str = "(nil)";
-printf("%s", str);
-break;
-}
-if ((form == 'c' || form == 'i' || form == 'f' || form == 's')
-&& format[i + 1])
-printf(", ");
-i++;
-}
-printf("/n");
-va_end(arg);
+  va_start(ar, format);
+  while (format != NULL && format[i])
+    {
+      form = format[i];
+      switch (form)
+	{
+	case 'c':
+	  printf("%c", va_arg(ar, int));
+	  break;
+	case 'i':
+	  printf("%d", va_arg(ar, int));
+	  break;
+	case 'f':
+	  printf("%f", va_arg(ar, double));
+	  break;
+	case 's':
+	  str = va_arg(ar, char *);
+	  if (str == NULL)
+	    str = "(nil)";
+	  printf("%s", str);
+	  break;
+	}
+      if ((form == 'c' || form == 'i' || form == 'f' || form == 's')
+	  && format[i + 1])
+	printf(", ");
+      i++;
+    }
+  printf("\n");
+  va_end(ar);
 }
