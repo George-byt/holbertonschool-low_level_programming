a333186acd96f0b84dc2c7cd1b46062803b1a08a
George-byt <jmoralesvalencia750@gmail.com>
2021-03-26 04:25:05 +0000

    Function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,13 +17,12 @@ return (0);
 
 for (i = 0; b[i] != '\0'; i++)
 {
-    if (b[i] != '0' && b[i] != '1')
-    return (0);
-    
+if (b[i] != '0' && b[i] != '1')
+return (0);
 num <<= 1;
 if (b[i] == '1')
 {
-    num = num ^ flip;
+num = num ^ flip;
 }
 
 }
