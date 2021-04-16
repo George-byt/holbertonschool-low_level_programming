b688f78236b46a1f0e02b9308128cb820516c95d
George-byt <jmoralesvalencia750@gmail.com>
2021-04-16 23:27:14 +0000

    file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * read_textfile - Read a file and print it in stdout
+ * @filename: Path of file.
+ * @letters: number of letter to print.
+ * Return: Number of letter it can read, 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buffer;
+int o_read, o_w, fd;
+read = o_w = fd = 0;
+
+if (filename)
+{
+fd = open(filename, O_RDONLY);
+if (fd != -1)
+{
+buffer = malloc(letters);
+if (buffer)
+{
+o_read = read(fd, buffer, letters);
+if (o_read != -1)
+o_w = write(STDOUT_FILENO, buffer, o_read);
+free(buffer);
+}
+close(fd);
+}
+}
+if (fd == -1 || o_read == -1 || o_w == -1)
+return (0);
+return (o_w);
+}
