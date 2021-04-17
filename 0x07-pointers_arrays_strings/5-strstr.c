6f3bd633ef4384fceec1ddd17f324fa4f8427634
George-byt <jmoralesvalencia750@gmail.com>
2021-04-17 04:37:16 +0000

    Even more pointers, arrays and strings.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,12 +11,27 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+unsigned int i, k, n;
+	char *p = '\0';
 
+	n = 0;
 
+	for (k = 0; needle[k] != '\0'; k++)
+	{
+	}
 
-
-
-
-
-
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (n == k)
+		{
+			p = &haystack[i - n];
+			return (p);
+		}
+		else if (haystack[i] == needle[n] && n < k)
+		{
+			n = n + 1;
+		} else
+			n = 0;
+	}
+	return (p);
 }
