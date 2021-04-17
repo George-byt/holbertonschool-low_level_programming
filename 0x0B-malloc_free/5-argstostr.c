ef4eb14943051ac758d067238d78b265362299f3
George-byt <jmoralesvalencia750@gmail.com>
2021-04-17 04:57:41 +0000

    malloc free

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,43 +1,64 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of your program
- * @ac: argc
- * @av: argv
- * Return: pointer to the new string
+ * _strlen - return the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of a string
+ **/
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * argstostr - Entry Point
+ * @ac: number of arguments passed to program
+ * @av: pointer to arguments passed to program
+ *
+ * Description: concatinate the arguments of the program
+ * Return: pointer to new string (Success), NULL (Failure)
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j;
+	int i, j, k, len, total_len;
 	char *p;
-	int c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
+	total_len = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			c++;
-		}
+		len = _strlen(av[i]);
+		total_len = len + 1;
 	}
-	p = malloc((sizeof(char) * c) + 1 + ac);
 
+	p = malloc((total_len + 1) * sizeof(char));
 	if (p == NULL)
+	{
+		free(p);
 		return (NULL);
-	c = 0;
+	}
+	/* use int k to count in p as 1D array for 2D info */
+	k = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			p[c] = av[i][j];
-			c++;
+			p[k] = av[i][j];
+			k++;
 		}
-		p[c] = '\n';
-		c++;
+		p[k] = '\n';
+		k++;
 	}
-	p[c] = '\0';
+	p[k] = '\0';
 	return (p);
 }
